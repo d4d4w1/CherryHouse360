1ac42d14dfc2c263e3c92a304ef71efc70aef924
d4d4w1 <andrea.gertosio@gmail.com>
2025-08-07 13:55:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,20 +15,6 @@
  */
 'use strict';
 
-// Inizio musica dopo primo clic o tocco
-function startBackgroundMusic() {
-  const audio = new Audio('music.mp3');
-  audio.loop = true;
-  audio.play();
-  document.removeEventListener('click', startBackgroundMusic);
-  document.removeEventListener('touchstart', startBackgroundMusic);
-}
-
-// Attendi prima interazione dell’utente
-document.addEventListener('click', startBackgroundMusic);
-document.addEventListener('touchstart', startBackgroundMusic);
-
-
 
 (function() {
   var Marzipano = window.Marzipano;
